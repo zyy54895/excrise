ce9d65eff31c231f425201a49d6c115ca20c5e23
zyy54895 <549334141@qq.com>
2018-03-05 11:48:12 +0800

    1

--- 99爬虫练习/02Urllib库基本使用.py
+++ 99爬虫练习/02Urllib库基本使用.py
@@ -4,7 +4,7 @@
 import requests
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 response = requests.get('http://222llll.com/xiaoshuo/jiqingwenxue/2017-06-01/29726.html', headers=headers)
-response.enconding = 'ISO8859-1'
+response.enconding = 'utf-8'
 #print(response.encoding)
 html = response.text
 
@@ -17,7 +17,7 @@ text = str(soup.p)
 # print(text1)
 # print(type(text1))
 
-f = open(title+'.txt', 'w', encoding='gbk')
+f = open(title+'.txt', 'w', encoding='utf-8')
 f.write(text)
 f.close()
 
