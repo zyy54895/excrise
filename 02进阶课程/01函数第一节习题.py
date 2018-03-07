d87230b7fca0fc127d9c9dd7848fb208541259a4
zyy54895 <549334141@qq.com>
2018-03-07 13:21:24 +0800

    Default

--- 02进阶课程/01函数第一节习题.py
+++ 02进阶课程/01函数第一节习题.py
@@ -28,7 +28,32 @@ def find_longest_len(*input_str):
     except AssertionError as e:
         return e
 
-result2 = find_longest_len(*('s', 12, 'qweqsawe', 'dfsd', 'as'))
+result2 = find_longest_len(*('s', 'qweqsawe', 'dfsd', 'as'))
 
-#
+#3.定义一个方法get_doc(module)，module参数为该脚本中导入或定义的模块对象，该函数返回module的帮助文档。
+# 例 print get_doc(urllib),则会输出urllib这个模块的帮助文档。
+def get_doc(module_name):
+    try:
+        exec('import ' + module_name)
+        print(help(module_name))
+    except ModuleNotFoundError as e:
+        print(e)
+get_doc('sys')
 
+#4.定义一个方法get_text(f),f参数为任意一个文件的磁盘路径，该函数返回f文件的内容。
+def get_text(f='C:/Users'):
+    import win32ui
+    dlg = win32ui.CreateFileDialog(1)  # 1表示打开文件对话框
+    dlg.SetOFNInitialDir('C:/Users')  # 设置打开文件对话框中的初始显示目录
+    dlg.DoModal()
+    f = dlg.GetPathName()  # 获取选择的文件名称
+    return f
+result4 = get_text()
+
+#5.定义一个方法get_dir(folder),folder参数为任意一个文件夹，该函数返回folder文件夹的文件列表。
+# 提示（可以了解python的glob模块）
+def get_dir(folder):
+    import glob
+    return glob.glob(folder+'./*')
+
+result5 = get_dir('C:/Users')
