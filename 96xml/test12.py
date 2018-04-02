7565876a7deedb97f9963a34bcdff80b16f313d7
zyy54895 <549334141@qq.com>
2018-04-02 21:50:06 +0800

    增加斗罗大陆1爬取

--- 96xml/test12.py
+++ 96xml/test12.py
@@ -1,23 +1,11 @@
 
 import re
 import os
-
-origin_path = 'C:/Users/zyy/66eeee.com/xiaoshuo/jiqingwenxue/'
-
-files = os.listdir(origin_path.encode('utf-8'))
-html_files = [str(files[i])[2:-1] for i in range(len(files))][4:]
-contents = []
-for i in range(len(html_files)):
-    with open(origin_path + html_files[i], 'r', encoding='utf-8') as file:
-        text = file.read()
-    lists = re.findall('<li>.*?href="(.*?)".*?title="(.*?)".*?</li>', text, re.S)
-    contents.extend(lists)
-contents_dict = dict(zip([contents[i][1] for i in range(len(contents))], [contents[i][0] for i in range(len(contents))]))
-
 from bs4 import BeautifulSoup
-for key in contents_dict.keys():
+from multiprocessing import Process,Queue,Pool
+def get_article(url):
     try:
-        with open(origin_path + contents_dict[key], 'r', encoding='utf-8') as html_read:
+        with open(url, 'r', encoding='utf-8') as html_read:
             text1 = html_read.read()
             soup = BeautifulSoup(text1, 'lxml')
             title = soup.title.string
@@ -25,8 +13,27 @@ for key in contents_dict.keys():
 
         with open('D:/soft/test/' + title + '.htm', 'w', encoding='utf-8') as html_write:
             html_write.write(article)
-    except OSError as e:
+    except:
+        print('%s error' % title)
+    finally:
         pass
-    except UnicodeDecodeError as u:
-        pass
+        # print('%s done' % title)
+
+if __name__ == '__main__':
+    origin_path = 'C:/Users/zyy/66eeee.com/xiaoshuo/jiqingwenxue/'
+    files = os.listdir(origin_path.encode('utf-8'))
+    html_files = [str(files[i])[2:-1] for i in range(len(files))][4:]
+    contents = []
+    for i in range(len(html_files)):
+        with open(origin_path + html_files[i], 'r', encoding='utf-8') as file:
+            text = file.read()
+        lists = re.findall('<li>.*?href="(.*?)".*?title="(.*?)".*?</li>', text, re.S)
+        contents.extend(lists)
+    contents_dict = dict(zip([contents[i][1] for i in range(len(contents))], [contents[i][0] for i in range(len(contents))]))
+    p = Pool(processes=5)
+    for key in contents_dict.keys():
+        p.apply_async(get_article, args=(origin_path+contents_dict[key], ))
+    p.close()
+    p.join()
+
 
